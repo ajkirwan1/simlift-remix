2efc894683c838d2b0baffe708d6c113a2ebb867
KasiaRaven26 <rejven777@gmail.com>
2025-01-26 19:21:06 +0200

    commitedonsunday

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,11 +2,15 @@
 
 export default function Header() {
   return (
-    <header>
-      <div>Logo</div>
-      <div>LINK 1</div>
-      <div>LINK 2</div>
-      <div>LINK 3</div>
+    <header className="flex justify-between items-center h-32 bg-blue-800">
+      <div className="ml-8">Logo</div>
+      <div className="flex mr-8 w-64 justify-around">
+        <ul>
+          <li>About</li>
+          <li>Products</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </header>
   );
 }
