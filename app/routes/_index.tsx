1996b25fac79a96d17dfb102ce3dce12e4e7a530
Adam Kirwan <ajkirwan1@gmail.com>
2025-01-28 20:15:49 +0000

    commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,9 +3,13 @@
 import type { MetaFunction } from "@remix-run/node";
 import heroImage from "/public/1picture.jpg";
 import glassesImage from "/public/glasses.jpg";
-import remixIcon from "/public/favicon.ico";
 import Header from "~/components/header";
 import { Navigation } from "swiper/modules";
+import liftS from "/public/products/SIMLIFT_S_small.jpg";
+import liftM from "/public/products/Simlift_M_small.jpg";
+import liftFlap from "/public/products/SimliftS_FLAP_small.jpg";
+import liftKlap from "/public/products/Simlift_S_z_klapą_small.jpg";
+import liftXL from "/public/products/SIMLIFT_XL_PRO_small.jpg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 // import { register } from "swiper/element/bundle";
@@ -22,7 +26,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <div className="fixed top-0 w-full z-10">
+      <div className="fixed top-0 w-full z-10 border-b-1">
         <Header />
       </div>
       <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
@@ -54,50 +58,54 @@ export default function Index() {
             </div>
           </div>
         </section>
-        <section className="relative w-full h-screen snap-center bg-sky-950 top-2/4">
-          <Swiper
-            className="h-48"
-            modules={[Navigation]}
-            slidesPerView={3}
-            navigation
-          >
-            <SwiperSlide>
-              <div className="flex h-full justify-center content-center">
-                <img src={remixIcon} alt=""></img>
-                <p>1</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="flex align-middle justify-center">
-              <div className="flex h-full justify-center content-center">
-                <img src={remixIcon} alt=""></img>
-                <p>2</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="flex align-middle justify-center">
-              <div className="flex h-full justify-center content-center">
-                <img src={remixIcon} alt=""></img>
-                <p>3</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="flex align-middle justify-center">
-              <div className="flex h-full justify-center content-center">
-                <img src={remixIcon} alt=""></img>
-                <p>4</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="flex align-middle justify-center">
-              <div className="flex h-full justify-center content-center">
-                <img src={remixIcon} alt=""></img>
-                <p>5</p>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className="flex align-middle justify-center">
-              <div className="flex justify-center">
-                <img src={remixIcon} alt=""></img>
-                <p>6</p>
-              </div>
-            </SwiperSlide>
-          </Swiper>
+        <section className="relative w-full h-screen snap-center">
+          <div className="h-20v bg-sky-950">
+            <Swiper
+              className="h-48"
+              modules={[Navigation]}
+              slidesPerView={1}
+              navigation
+            >
+              <SwiperSlide>
+                <div className="flex h-full justify-center content-center">
+                  <img src={liftS} alt=""></img>
+                  <div className="flex flex-col basis-1/4">
+                    <h2>SIMLIFT S</h2>
+                    <p>
+                      Electric projector lift recommended for small size
+                      Business projectors.Lift with projector requires 15 cm
+                      distance between ceiling and false ceiling.Projector power
+                      and signal cables are secured wit Easy-chain ® guide.
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="flex align-middle justify-center">
+                <div className="flex h-full justify-center content-center">
+                  <img src={liftM} alt=""></img>
+                  <p>2</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="flex align-middle justify-center">
+                <div className="flex h-full justify-center content-center">
+                  <img src={liftFlap} alt=""></img>
+                  <p>3</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="flex align-middle justify-center">
+                <div className="flex h-full justify-center content-center">
+                  <img src={liftKlap} alt=""></img>
+                  <p>4</p>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className="flex align-middle justify-center">
+                <div className="flex h-full justify-center content-center">
+                  <img src={liftXL} alt=""></img>
+                  <p>5</p>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </section>
       </div>
 
