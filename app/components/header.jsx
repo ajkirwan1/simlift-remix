aaad58de61cb762d888c93f1b6a92f4b57a0fe67
Adam Kirwan <ajkirwan1@gmail.com>
2025-02-09 13:46:34 +0000

    commit

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="bg-gradient-to-r from-simlift-left to-simlift-right flex justify-between items-center h-20v w-screen text-white font-thin ">
       <NavLink to="/" className="flex items-center h-full">
-        <img alt="" src={logo} className="ml-8 h-1/2" />
+        <img alt="" src={logo} className="ml-8 h-[40%]" />
       </NavLink>
       <ul className="flex mr-8 w-1/3 justify-around list-none">
         <li className="text-xl hover:underline decoration-1">
