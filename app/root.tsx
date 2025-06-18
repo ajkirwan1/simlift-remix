33c9b3cf2055ea9082621272bc80f1a8aa567eb4
Adam Kirwan <ajkirwan1@gmail.com>
2025-06-18 21:59:01 +0100

    removed crossorigin from root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -13,43 +13,42 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
 
 import "@fontsource-variable/nunito-sans";
+
 import "./tailwind.css";
 
-export function meta() {
-  return [
-    { charset: "utf-8" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-  ];
-}
-
-export default function App() {
-  useEffect(() => {
-    i18n.init();
-  }, []);
-
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
           rel="stylesheet"
         />
       </head>
       <body>
-        <I18nextProvider i18n={i18n}>
-          <Outlet />
-        </I18nextProvider>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  useEffect(() => {
+    // initialize i18next on client side
+    i18n.init();
+  }, []);
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Outlet />
+    </I18nextProvider>
   );
 }
